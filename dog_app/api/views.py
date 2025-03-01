a3c35c9b8c055eb5b5160365881b390ce66446bb
bissaliev <bissaliev21@gmail.com>
2025-03-01 10:57:45 +0400

    Добавил вьюсеты DogViewSet, BreedViewSet для представления пород и собак

--- dog_app/api/views.py
+++ dog_app/api/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from api.serializers import BreedSerializer, DogSerializer
+from django.db.models import Avg, Count
+from dogs.models import Breed, Dog
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+
+class DogViewSet(ModelViewSet):
+    queryset = Dog.objects.all()
+    serializer_class = DogSerializer
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        if self.action == "list":
+            queryset = queryset.annotate(avg_breed_age=Avg("breed__dogs__age"))
+        if self.action == "retrieve":
+            queryset = queryset.annotate(
+                count_breed_dogs=Count("breed__dogs__id")
+            )
+        return queryset
+
+
+class BreedViewSet(ModelViewSet):
+    queryset = Breed.objects.all()
+    serializer_class = BreedSerializer
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        queryset = queryset.annotate(count_dogs=Count("dogs"))
+        return queryset
