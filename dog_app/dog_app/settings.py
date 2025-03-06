b298c291e28464a13626e4b2aebb1363758a949b
bissaliev <bissaliev21@gmail.com>
2025-03-06 11:14:41 +0400

    Добавил настройки пагинации в settings.py

--- dog_app/dog_app/settings.py
+++ dog_app/dog_app/settings.py
@@ -101,3 +101,9 @@ STATIC_URL = "static/"
 STATIC_ROOT = "collectstatic"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+REST_FRAMEWORK = {
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 10,
+}
